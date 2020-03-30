09b38bee65243234c0dac1b124ddc15ec79055e8
rahuldkjain <rahuldkjain@gmail.com>
2020-03-30 17:52:11 +0530

    page titles

--- src/router/index.js
+++ src/router/index.js
@@ -10,14 +10,16 @@ import FavMovies from '@/components/FavMovies'
 import Test from '@/components/subComponents/card'
 
 Vue.use(Router)
-
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: Home,
+      meta: {
+        title: 'RJ Portfolio'
+      }
     },
     {
       path: '/test',
@@ -27,32 +29,55 @@ export default new Router({
     {
       path: '/work',
       name: 'Work',
-      component: Work
+      component: Work,
+      meta: {
+        title: 'Work: RJ'
+      }
     },
     {
       path: '/awards',
       name: 'Awards',
-      component: Awards
+      component: Awards,
+      meta: {
+        title: 'Awards: RJ'
+      }
     },
     {
       path: '/favourites',
       name: 'Favourites',
-      component: Favourites
+      component: Favourites,
+      meta: {
+        title: 'Favourites: RJ'
+      }
     },
     {
       path: '/favourites/movies',
       name: 'FavMovies',
-      component: FavMovies
+      component: FavMovies,
+      meta: {
+        title: 'Fav Movies'
+      }
     },
     {
       path: '/favourites/tv',
       name: 'FavTvs',
-      component: FavTvs
+      component: FavTvs,
+      meta: {
+        title: 'Fav TVs'
+      }
     },
     {
       path: '/favourites/books',
       name: 'FavBooks',
-      component: FavBooks
+      component: FavBooks,
+      meta: {
+        title: 'Fav Books'
+      }
     }
   ]
 })
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
+})
+export default router;
